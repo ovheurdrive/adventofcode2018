894fb8e18b25e71935764be2ae2dadf9c984348e
Stanislas Plessia <stanislas.plessia@streem.com.au>
2018-12-01 12:55:14 +0100

    [WIP] Day1 - part 2

---
+++ day-01/part 2/src/main.rs
@@ -0,0 +1,30 @@
+use std::fs;
+
+fn main(){
+    let mut current_f = 0;
+    let mut res = i32::max_value();
+    
+    let input = fs::read_to_string("./input.txt").expect("File not found");
+
+    let mut frequencies: Vec<i32> = Vec::new();
+    frequencies.push(current_f);
+
+    while res == i32::max_value() {
+        let vec = input.split("\n");
+        for s in vec {
+            let val = s.parse::<i32>().unwrap();
+            current_f += val;
+
+            if frequencies.contains(&current_f) == false {
+                frequencies.push(current_f);
+            }
+            else {
+                res = current_f;
+            }
+        }
+        //println!("{:?}", frequencies);
+    }
+    
+    println!("{}",res);
+    
+}
