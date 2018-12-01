79efbebdc3b58ae242f76144c8b95eb2d85ca207
Stanislas Plessia <stanislas.plessia@streem.com.au>
2018-12-01 13:03:22 +0100

    [Fix] Day1 - part 2

--- day-01/part 2/src/main.rs
+++ day-01/part 2/src/main.rs
@@ -3,7 +3,7 @@ use std::fs;
 fn main(){
     let mut current_f = 0;
     let mut res = i32::max_value();
-    
+
     let input = fs::read_to_string("./input.txt").expect("File not found");
 
     let mut frequencies: Vec<i32> = Vec::new();
@@ -15,11 +15,12 @@ fn main(){
             let val = s.parse::<i32>().unwrap();
             current_f += val;
 
-            if frequencies.contains(&current_f) == false {
+            if !frequencies.contains(&current_f) {
                 frequencies.push(current_f);
             }
             else {
                 res = current_f;
+                break;
             }
         }
         //println!("{:?}", frequencies);
